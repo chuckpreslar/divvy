1bc46f1a9cd7d4401538f66ba3f6226ea5e4b1f3
Chuck Preslar <chuckpreslar@gmail.com>
2013-06-01 11:30:06 -0400

    New naming.

--- divvy.go
+++ divvy.go
@@ -4,122 +4,122 @@ import (
   _ "fmt"
 )
 
-type FlexArray []interface{}
+type Divvy []interface{}
 
-func NewFlexArray() *FlexArray {
-  return &FlexArray{}
+func New() *Divvy {
+  return &Divvy{}
 }
 
-func (f *FlexArray) InsertAt(item interface{}, index int) *FlexArray {
-  temp := *f
+func (d *Divvy) InsertAt(item interface{}, index int) *Divvy {
+  temp := *d
   temp = append(temp[:index], append([]interface{}{item}, temp[index:]...)...)
-  *f = temp
-  return f
+  *d = temp
+  return d
 }
 
-func (f *FlexArray) RemoveAt(index int) *FlexArray {
-  temp := *f
+func (d *Divvy) RemoveAt(index int) *Divvy {
+  temp := *d
   copy(temp[index:], temp[index+1:])
   temp[len(temp)-1] = nil
   temp = temp[:len(temp)-1]
-  *f = temp
-  return f
+  *d = temp
+  return d
 }
 
-func (f *FlexArray) Splice(index, count int) *FlexArray {
+func (d *Divvy) Splice(index, count int) *Divvy {
   for i := 0; i < count; i += 1 {
-    f.RemoveAt(index)
+    d.RemoveAt(index)
   }
-  return f
+  return d
 }
 
-func (f *FlexArray) Append(items ...interface{}) *FlexArray {
-  temp := append(*f, items...)
-  *f = temp
-  return f
+func (d *Divvy) Append(items ...interface{}) *Divvy {
+  temp := append(*d, items...)
+  *d = temp
+  return d
 }
 
-func (f *FlexArray) Prepend(items ...interface{}) *FlexArray {
-  temp := append(items, *f...)
-  *f = temp
-  return f
+func (d *Divvy) Prepend(items ...interface{}) *Divvy {
+  temp := append(items, *d...)
+  *d = temp
+  return d
 }
 
-func (f *FlexArray) Push(items ...interface{}) *FlexArray {
-  return f.Append(items...)
+func (d *Divvy) Push(items ...interface{}) *Divvy {
+  return d.Append(items...)
 }
 
-func (f *FlexArray) Pop() interface{} {
-  temp := *f
+func (d *Divvy) Pop() interface{} {
+  temp := *d
   index := len(temp) - 1
   item := temp[index]
-  *f = temp[:index]
+  *d = temp[:index]
   return item
 }
 
-func (f *FlexArray) Queue(items ...interface{}) *FlexArray {
-  return f.Append(items...)
+func (d *Divvy) Queue(items ...interface{}) *Divvy {
+  return d.Append(items...)
 }
 
-func (f *FlexArray) Dequeue() interface{} {
-  temp := *f
+func (d *Divvy) Dequeue() interface{} {
+  temp := *d
   item := temp[0]
-  *f = temp[1:]
+  *d = temp[1:]
   return item
 }
 
-func (f *FlexArray) Each(fn func(interface{})) *FlexArray {
-  temp := *f
+func (d *Divvy) Each(fn func(interface{})) *Divvy {
+  temp := *d
   for _, item := range temp {
     fn(item)
   }
-  return f
+  return d
 }
 
-func (f *FlexArray) EachWithIndex(fn func(interface{}, int)) *FlexArray {
-  temp := *f
+func (d *Divvy) EachWithIndex(fn func(interface{}, int)) *Divvy {
+  temp := *d
   for index, item := range temp {
     fn(item, index)
   }
-  return f
+  return d
 }
 
-func (f *FlexArray) Map(fn func(interface{}) interface{}) *FlexArray {
-  temp := *f
+func (d *Divvy) Map(fn func(interface{}) interface{}) *Divvy {
+  temp := *d
   for index, item := range temp {
     temp[index] = fn(item)
   }
-  *f = temp
-  return f
+  *d = temp
+  return d
 }
 
-func (f *FlexArray) Select(fn func(interface{}) bool) *FlexArray {
-  temp := *f
+func (d *Divvy) Select(fn func(interface{}) bool) *Divvy {
+  temp := *d
   selection := []interface{}{}
   for _, item := range temp {
     if fn(item) {
       selection = append(selection, item)
     }
   }
-  result := FlexArray(selection)
+  result := Divvy(selection)
   return &result
 }
 
-func (f *FlexArray) Reject(fn func(interface{}) bool) *FlexArray {
-  temp := *f
+func (d *Divvy) Reject(fn func(interface{}) bool) *Divvy {
+  temp := *d
   rejection := []interface{}{}
   for _, item := range temp {
     if !fn(item) {
       rejection = append(rejection, item)
     }
   }
-  result := FlexArray(rejection)
+  result := Divvy(rejection)
   return &result
 }
 
-func (f *FlexArray) IndexOf(item interface{}) int {
+func (d *Divvy) IndexOf(item interface{}) int {
   index := -1
-  temp := *f
+  temp := *d
   for i, v := range temp {
     if v == item {
       index = i
@@ -129,12 +129,12 @@ func (f *FlexArray) IndexOf(item interface{}) int {
   return index
 }
 
-func (f *FlexArray) LastIndexOf(item interface{}) int {
-  index := f.IndexOf(item)
+func (d *Divvy) LastIndexOf(item interface{}) int {
+  index := d.IndexOf(item)
   if ^index == 0 {
     return index
   } else {
-    temp := *f
+    temp := *d
     for i := index; i < len(temp); i += 1 {
       if item == temp[i] {
         index = i
